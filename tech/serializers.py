47a292eda5feb6ed9a38dcc41f59ac68cae8ada5
Eraldo Jr <eraldogoncalvesjr@gmail.com>
2017-09-06 21:34:56 -0300

    removed tech folder

--- tech/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Event
-
-class EventSerializer(serializer.ModelSerializer):
-
-    class Meta:
-        model = Event
-        # fields = '__all__'
-        fields = ('id', 'title', 'description')
--- tech/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-
-from django.contrib import admin
-
-import tech.views
-
-urlpatterns = [
-    url(r'^', tech.views.index, name='index'),
-]
